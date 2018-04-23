4ba12f751861d3206fe9ddec28be382c70294ce0
Feynman Liang <feynman.liang@gmail.com>
2018-04-22 23:08:05 -0700

    Fix make and run

--- make_and_run.sh
+++ make_and_run.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-make $1 && mpirun --hostfile hostfile -n 2 ./bin/$1 && python view_samples.py && python view_latencies.py
+cd build \
+    && rm *.mm\
+    ; make -j4 $1 \
+    && cd ../ \
+    && mpirun -hostfile hostfile -n 4 ./build/bin/$1 \
+    && python view_samples.py\
+    ; python view_latencies.py
